79cc00adc62cec953877cf416d09a002ba1dafae
marianadiazl <a01252731@itesm.mx>
2021-10-01 17:24:44 +0000

    m

--- assignments/16DiagonalPrincipal/src/exercise.py
+++ assignments/16DiagonalPrincipal/src/exercise.py
@@ -1,5 +1,23 @@
+def matriz(lista) :
+    diagonal = []
+    for i in range(len(lista)) :
+        diagonal.append(lista[i][i])
+    return(diagonal)
 
 def main():
+    lista = []
+    fila = int(input(''))
+    columna = int(input(''))
+    if fila == columna :
+        for i in range(fila) :
+            lista.append([])
+            for j in range(columna) :
+                n = int(input())
+                lista[i].append(n)
+        resultado = matriz(lista)
+        print(resultado)
+    else :
+        print('La matriz no es cuadrada')
   
 
 if __name__=='__main__':
